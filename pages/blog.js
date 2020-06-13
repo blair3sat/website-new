7393470bd6e162dad0c19f1983394ac30e911c43
Jbolt01 <vrswizards2015@gmail.com>
2020-06-13 15:47:28 -0400

    Dynamic Importing

--- pages/blog.js
+++ pages/blog.js
@@ -5,10 +5,10 @@ import Nav from "../components/nav";
 
 import postsData from "../posts";
 
-// Dynamically import the blog posts
-// postsData.forEach(post => {
-// 	post.file = import("../pages" + post.filePath.replace("pages", ""));
-// });
+ //Dynamically import the blog posts
+ postsData.forEach(post => {
+ 	post.file = import("../pages" + post.filePath.replace("pages", ""));
+ });
 
 const PostPreview = styled.div`
   border: 1.15px solid rgba(0, 0, 0, 0.15);
@@ -152,37 +152,37 @@ const blogPage = ({ posts = postsData }) => (
 
 // Before page loads await the dynamic components. prevents blog preview page flash.
 blogPage.getInitialProps = async () => {
-  // await Promise.all(
-  // 	postsData.map(async post => {
-  // 		const file = await post.file;
-  // 		post.BlogPost = file.default;
-  // 		post.meta = file.meta;
-  // 		return post;
-  // 	})
-  // );
+   await Promise.all(
+   	postsData.map(async post => {
+   		const file = await post.file;
+   		post.BlogPost = file.default;
+   		post.meta = file.meta;
+   		return post;
+   	})
+   );
 
-  postsData.forEach(a => {
-    a.meta = {
-      author: "Andrew Lisowski",
-      authorLink: "https://github.intuit.com/alisowski",
-      avatar: "https://avatars2.githubusercontent.com/u/1192452?s=400&v=4",
-      publishDate: "2018-05-10T12:00:00Z",
-      title: "First Post"
-    };
-    a.BlogPost = () => (
-      <div>
-        <h1>Hello</h1>
-        <p>lorem ipusm dolor sit amet</p>
-        <p>asdfasdf</p>
-        <h2>asdfasdfasd</h2>
-        <h2>asdfasdfasd</h2>
-        <h2>asdfasdfasd</h2>
-        <h2>asdfasdfasd</h2>
-        <h2>asdfasdfasd</h2>
-        <h2>asdfasdfasd</h2>
-      </div>
-    );
-  });
+  //postsData.forEach(a => {
+  //  a.meta = {
+  //    author: "Andrew Lisowski",
+  //    authorLink: "https://github.intuit.com/alisowski",
+  //    avatar: "https://avatars2.githubusercontent.com/u/1192452?s=400&v=4",
+  //    publishDate: "2018-05-10T12:00:00Z",
+  //    title: "First Post"
+  //  };
+  //  a.BlogPost = () => (
+  //    <div>
+  //      <h1>Hello</h1>
+  //      <p>lorem ipusm dolor sit amet</p>
+  //      <p>asdfasdf</p>
+  //     <h2>asdfasdfasd</h2>
+  //      <h2>asdfasdfasd</h2>
+  //      <h2>asdfasdfasd</h2>
+  //      <h2>asdfasdfasd</h2>
+  //      <h2>asdfasdfasd</h2>
+  //      <h2>asdfasdfasd</h2>
+  //    </div>
+  //  );
+  //});
 
   return { posts: [...postsData] };
 };
